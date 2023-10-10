96383eaa9855a49edc296f1501344e06f278d74c
Trần Minh Quân <21130494@st.hcmuaf.edu.vn>
2023-10-10 09:22:57 +0700

    Lab 4 - Bài 1

---
+++ lab4/ex1/script.js
@@ -0,0 +1,15 @@
+document.getElementById('red-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('green-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('blue-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('yellow-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'yellow';
+});
